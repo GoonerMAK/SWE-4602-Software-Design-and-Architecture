383a77c5324a07b885cd2efa8534baa5c92ea123
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2024-04-24 22:01:49 +0600

    Microservice implementation

---
+++ Lab_3/order-service/src/main/java/com/example/orderservice/valueObject/ResponseValueObject.java
@@ -0,0 +1,25 @@
+package com.example.orderservice.valueObject;
+
+import com.example.orderservice.entity.Order;
+
+
+public class ResponseValueObject
+{
+    private Customer customer;
+    private Product product;
+    private Employee employee;
+    private Order order;
+
+
+    public void setCustomer(Customer customer) {
+    }
+
+    public void setProduct(Product product) {
+    }
+
+    public void setEmployee(Employee employee) {
+    }
+
+    public void setOrder(Order order) {
+    }
+}
